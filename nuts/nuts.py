e198fd631b6a8dae5a0119eb8f091d63b07bde09
agmuth <agmuth@gmail.com>
2024-01-10 23:46:39 -0700

    bump

---
+++ nuts/nuts.py
@@ -0,0 +1,25 @@
+
+
+class NoUTurnSampler:
+    
+    def __init__(self):
+        pass
+    
+    def __call__(self):
+        pass
+    
+    @staticmethod
+    def _build_tree():
+        pass
+    
+    
+    @staticmethod
+    def _leapfrog():
+        pass
+
+
+
+
+
+if __name__ == "__main__":
+    print("NUTS")
