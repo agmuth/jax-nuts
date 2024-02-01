55ad47fee2f61d9cadc407c08ca75eb95b254e46
agmuth <agmuth@gmail.com>
2024-01-31 20:32:02 -0700

    failing when prec is param

--- tests/conjugate_priors.py
+++ tests/conjugate_priors.py
@@ -1,3 +1,4 @@
+import jax
 import jax.numpy as jnp
 
 # ref: https://www.johndcook.com/CompendiumOfConjugatePriors.pdf
@@ -20,7 +21,7 @@ class NormalProcessPrecisonKnown:
     def __call__(self, theta):
         # posterior loglik for mu
         mu = theta[0]
-        loglik = 0
+        loglik = 0.0
         loglik += 0.5 * jnp.log(self.p_prime)
         loglik += (
             -0.5 * self.p_prime * jnp.dot((mu - self.m_prime), (mu - self.m_prime))
@@ -28,5 +29,32 @@ class NormalProcessPrecisonKnown:
         return loglik
 
     @property
-    def posterior_params(self):
-        return self.m_prime, self.p_prime
+    def posterior_mean(self):
+        return self.m_prime
+
+
+class NormalProcessMeanKnown:
+    def __init__(self, ss, n, mu, a, b):
+        self.ss = ss
+        self.n = n
+        self.mu = mu  # mean
+        # priors for rho
+        self.a = a
+        self.b = b
+
+        self.a_prime = self.a + self.n / 2
+        self.b_prime = self.b + self.ss / 2
+
+    def __call__(self, theta):
+        # posterior loglik for rho
+        rho = theta[0]
+        loglik = 0.0
+        loglik += rho ** (self.a_prime - 1)
+        loglik *= jnp.exp(-rho / self.b_prime)
+        loglik /= jax.scipy.special.gamma(self.a_prime)
+        loglik /= self.b_prime**self.a_prime
+        return loglik
+
+    @property
+    def posterior_mean(self):
+        return self.a_prime * self.b_prime
--- tests/test_normal_process_mean_known.py
+++ tests/test_normal_process_mean_known.py
@@ -5,15 +5,15 @@ import numpy as np
 import pytest
 
 from jaxnuts.nuts import NoUTurnSampler
-from tests.conjugate_priors import NormalProcessPrecisonKnown
+from tests.conjugate_priors import NormalProcessMeanKnown
 
 
 @dataclass
 class ProblemInstance:
-    m: float
-    p: float
-    rho: float
-    x_bar: float
+    a: float
+    b: float
+    mu: float
+    ss: float
     n: int
 
     def dict(self):
@@ -22,22 +22,26 @@ class ProblemInstance:
 
 problems = [
     ProblemInstance(
-        m=2.0,
-        p=1.0,
-        rho=2.0,
-        x_bar=4.0,
-        n=10,
+        a=3.0,
+        b=1.5,
+        mu=2.0,
+        ss=38.0,
+        n=20,
     )
 ]
 
 
 @pytest.mark.parametrize("problem", problems)
-def test_normal_process_precison_known(problem: ProblemInstance):
-    cp = NormalProcessPrecisonKnown(**problem.dict())
+def test_normal_process_mean_known(problem: ProblemInstance):
+    cp = NormalProcessMeanKnown(**problem.dict())
     nuts = NoUTurnSampler(loglik=cp)
-    theta_0 = jnp.array([0.0])
+    theta_0 = jnp.array([1.0])
     M = 200
     theta_samples = nuts(theta_0, M)
     theta_samples = theta_samples[M // 2 :]
     nuts_posterior_mean = theta_samples.mean()
     assert np.isclose(cp.posterior_mean, nuts_posterior_mean, rtol=0.1)
+
+
+# if __name__ == "__main__":
+#     test_normal_process_mean_known(problems[0])
